63d78fb6f5c678e90db10f3b408fdc14a8e3dc73
Eli Mallon <eli@livepeer.org>
2023-07-02 17:49:01 -0700

    iameli-streams: working!

--- iameli-streams/src/index.tsx
+++ iameli-streams/src/index.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  Player,
+  createReactClient,
+  studioProvider,
+  LivepeerConfig,
+} from "@livepeer/react";
+import styled from "styled-components";
 
 const div = document.createElement("div");
 div.innerHTML = "hello";
 div.id = "iameli-streams";
 document.body.prepend(div);
 
-const App = () => <h1>Eli's Livestream App!!!</h1>;
+const livepeerClient = createReactClient({
+  provider: studioProvider({ apiKey: "" }),
+});
+
+const Container = styled.div`
+  max-width: 1170px;
+  margin: auto;
+`;
+
+const App = () => {
+  return (
+    <Container>
+      <LivepeerConfig client={livepeerClient}>
+        <Player playbackId="d649rj83prk16y67" autoPlay={true}></Player>
+      </LivepeerConfig>
+    </Container>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector("#iameli-streams"));
