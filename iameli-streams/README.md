9a88196478c02b153588cbfa2f87ce3774f29a7c
Eli Mallon <eli@livepeer.org>
2023-07-06 13:13:11 -0700

    iameli-streams: add lowLatency picker

---
+++ iameli-streams/README.md
@@ -0,0 +1,6 @@
+```javascript
+script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "http://localhost:8765/iameli-streams.js";
+document.getElementsByTagName("head")[0].appendChild(script);
+```
--- iameli-streams/src/index.tsx
+++ iameli-streams/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import {
   Player,
@@ -22,14 +22,49 @@ const Container = styled.div`
   margin: auto;
 `;
 
+const LOCAL_STORAGE_LOW_LATENCY_KEY = "LOW_LATENCY"
+
+let lowLatencyDefault = true;
+try {
+  const val = localStorage.getItem(LOCAL_STORAGE_LOW_LATENCY_KEY);
+  if (val === "true") {
+    lowLatencyDefault = true;
+  } else if (val === "false") {
+    lowLatencyDefault = false;
+  }
+}
+catch (e) {
+  // oh well
+}
+
+const LLButton = styled.button`
+  margin: 5px auto;
+  font-size: 1.4em;
+  display: block;
+`;
+
 const App = () => {
+  const [lowLatency, setLowLatency] = useState(lowLatencyDefault);
   return (
     <Container>
       <LivepeerConfig client={livepeerClient}>
-        <Player playbackId="d649rj83prk16y67" autoPlay={true} lowLatency={true}></Player>
+        <Player playbackId="d649rj83prk16y67" autoPlay={true} lowLatency={lowLatency}></Player>
       </LivepeerConfig>
+      <LLButton className="button logo-button" onClick={() => {
+        const newState = !lowLatency
+        setLowLatency(newState)
+        try {
+          localStorage.setItem(LOCAL_STORAGE_LOW_LATENCY_KEY, newState ? "true" : "false")
+        } catch (e) {
+          // oh well oh well oh well
+        }
+      }}>
+        {lowLatency ? "Disable" : "Enable"} Low-Latency (WebRTC)
+      </LLButton>
     </Container>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#iameli-streams"));
+if (!document.location.hash.includes("novid")) {
+  ReactDOM.render(<App />, document.querySelector("#iameli-streams"));
+}
